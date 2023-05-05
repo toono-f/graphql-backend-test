985961e3688b01a32980b6a8ebdbfbcfec284e31
toono-f <52028799+toono-f@users.noreply.github.com>
2023-05-05 18:25:34 +0900

    queryとmutationの追加

--- src/server.js
+++ src/server.js
@@ -1,21 +1,39 @@
 const { ApolloServer, gql } = require("apollo-server");
+const fs = require("fs");
+const path = require("path");
 
-// graphlql schema
-const typeDefs = gql`
-  type Query {
-    info: String!
-  }
-`;
+// hackernewsの1つ１つの投稿
+let links = [
+  {
+    id: "link-0",
+    description: "Fullstack tutorial for GraphQL",
+    url: "https://www.howtographql.com/",
+  },
+];
 
 // resolver
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
+    feed: () => links,
+  },
+
+  Mutation: {
+    post: (parent, args) => {
+      let idCount = links.length;
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url,
+      };
+      links.push(link);
+      return link;
+    },
   },
 };
 
 const server = new ApolloServer({
-  typeDefs,
+  typeDefs: fs.readFileSync(path.join(__dirname, "schema.graphql"), "utf8"),
   resolvers,
 });
 
