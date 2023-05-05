dcdc993e65f1f0a4a636f7ffb4d388bca2ae53bc
toono-f <52028799+toono-f@users.noreply.github.com>
2023-05-05 17:47:02 +0900

    サーバー起動

---
+++ src/server.js
@@ -0,0 +1,22 @@
+const { ApolloServer, gql } = require("apollo-server");
+
+// graphlql schema
+const typeDefs = gql`
+  type Query {
+    info: String!
+  }
+`;
+
+// resolver
+const resolvers = {
+  Query: {
+    info: () => `This is the API of a Hackernews Clone`,
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server.listen().then(({ url }) => console.log(`Server is running on ${url}`));
